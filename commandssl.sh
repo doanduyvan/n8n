b7e32581ed750e7bc8b1972ba9b50b81227a8f63
doanduyvan <duyvanlee2001@gmail.com>
2026-04-16 22:47:22 +0700

    up

--- commandssl.sh
+++ commandssl.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-docker compose run --rm certbot certonly \
+# lệnh này dùng để chạy lần đầu dùng để cấp ssl.
+docker compose run --rm --entrypoint "" certbot certonly \
   --webroot \
   --webroot-path=/var/www/certbot \
   -d odinn.site \
   --email duyvanlee2001@gmail.com \
   --agree-tos \
-  --no-eff-email
+  --no-eff-email -v
+
+
+# docker compose run --rm certbot certonly \
+#   --webroot \
+#   --webroot-path=/var/www/certbot \
+#   -d odinn.site \
+#   --email duyvanlee2001@gmail.com \
+#   --agree-tos \
+#   --no-eff-email
